22514dd39d73c32d62f2fd7e75da627e61501085
Matej <klancar.matej@gmail.com>
2023-07-08 18:53:22 +0200

    Rekonstrukcija slik z besedilom

--- Program/slikaTekst.m
+++ Program/slikaTekst.m
@@ -1,26 +1,24 @@
-img = imread('knjigaTekst.tiff');
-img = img(:,:,1:3);
+img = imread('mestoTekst.png');
 
 img = rgb2gray(img);
 norma = norm(cast(img,"double"), "fro")
 [n1, n2] = size(img)
 A = zeros(n1, n2);
-M = zeros(n1, n2);
+M = ones(n1, n2);
 
 for i = 1:n1
     for j = 1:n2
         A(i, j) = img(i, j);
-        M(i, j) = 1;
         if(img(i, j) == 0)
             M(i, j) = 0;
         end
     end
 end
 
-% tic
+tic
 Y = solver(sparse(A), sparse(M), "lmafit", 16);
 casIzvajanja = toc
-orgSlika = rgb2gray(imread('knjiga.jpg'));
+orgSlika = rgb2gray(imread('mesto.jpg'));
 napaka = norm(Y - cast(orgSlika,"double") , "fro")
 
 % bestNapaka = inf
